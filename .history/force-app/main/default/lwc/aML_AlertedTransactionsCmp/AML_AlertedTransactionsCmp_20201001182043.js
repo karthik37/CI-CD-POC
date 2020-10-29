ea40ef7c8f20f6089e44b8c5381d87459e2c8e51
Karthik Matam <18894@ITCINFOTECH.com>
2020-10-29 20:35:29 +0530

    commit test

---
+++ .history/force-app/main/default/lwc/aML_AlertedTransactionsCmp/AML_AlertedTransactionsCmp_20201001182043.js
@@ -0,0 +1,16 @@
+import { LightningElement,track,wire,api } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import displayTransactionBoRecords from'@salesforce/apex/displayTransactionBoController.displayTransactionBoRecords';
+export default class DisplayTransactionBo extends LightningElement {
+@api recordId;
+@track columns = [{label:'Seller Oracle Id',fieldName:'Seller_Oracle_id__c',type:'text',sortable: true},
+{label:'Buyer Oracle Id',fieldName:'Buyer_Oracle_Id__c',type:'text',sortable: true},
+{label:'Transaction Reference Number',fieldName:'Transaction_Reference_Number__c',type:'text',sortable: true},
+{label:'Transaction Date',fieldName:'Transaction_Date__c',type:'date',sortable: true},
+{label:'Transaction Currency Amount',fieldName:'Transaction_Currency_Amount__c',type:'number',sortable: true},
+{label:'Transaction USD Amount',fieldName:'Transaction_USD_Amount__c',type:'number',sortable: true}
+];
+@wire(displayTransactionBoRecords,{
+accId:'$recordId'
+}) transactions;
+}
---
+++ .history/force-app/main/default/lwc/aML_AlertedTransactionsCmp/AML_AlertedTransactionsCmp_20201001182439.js
@@ -0,0 +1,16 @@
+import { LightningElement,track,wire,api } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import fetchAlertedTransactions from'@salesforce/apex/AML_ALertedTransactionsController.fetchAlertedTransactions';
+export default class ShowAlertedTransactions extends LightningElement {
+@api recordId;
+@track columns = [{label:'Seller Oracle Id',fieldName:'Seller_Oracle_id__c',type:'text',sortable: true},
+{label:'Buyer Oracle Id',fieldName:'Buyer_Oracle_Id__c',type:'text',sortable: true},
+{label:'Transaction Reference Number',fieldName:'Transaction_Reference_Number__c',type:'text',sortable: true},
+{label:'Transaction Date',fieldName:'Transaction_Date__c',type:'date',sortable: true},
+{label:'Transaction Currency Amount',fieldName:'Transaction_Currency_Amount__c',type:'number',sortable: true},
+{label:'Transaction USD Amount',fieldName:'Transaction_USD_Amount__c',type:'number',sortable: true}
+];
+@wire(fetchAlertedTransactions,{
+    alertId:'$recordId'
+}) transactions;
+}
